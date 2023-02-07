d0c99db2b4f746035b95b2e2fd3d59be29d5757b
bw00k <bw00k@naver.com>
2023-02-07 17:38:04 +0900

    무사히

---
+++ HelloApp/src/main/java/com/yedam/member/command/SignOnFormControl.java
@@ -0,0 +1,26 @@
+package com.yedam.member.command;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.yedam.common.Command;
+
+public class SignOnFormControl implements Command {
+
+	@Override
+	public void exec(HttpServletRequest req, HttpServletResponse resp) {
+		// TODO Auto-generated method stub
+		// signon.jsp
+		
+		try {
+			req.getRequestDispatcher("WEB-INF/member/signon.jsp").forward(req,resp);
+		} catch (ServletException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
